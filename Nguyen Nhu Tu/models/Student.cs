9dd08e2095b958ca51e00e85832eddf67b032a64
Nguyen Nhu Tu <tunguyennhuxyz@gmail.com>
2023-12-13 15:19:27 +0700

    Create Students.cs in folder models

---
+++ Nguyen Nhu Tu/models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nguyen_Nhu_Tu.models
+{
+    internal class Student
+    {
+    }
+}
